48596f9d48059982d3afc5806048c12f9edba4a9
EdhuMS <edhums10@gmail.com>
2025-08-30 15:15:58 -0500

    productos new JSON, fix responsive

--- app/productos.js
+++ app/productos.js
@@ -1,6 +1,6 @@
 let todosLosProductos = [];
 let productosFiltrados = [];
-const productosPorPagina = 20;
+const productosPorPagina = 16;
 let paginaActual = 1;
 
 function renderProductos(lista, contenedorId) {
@@ -15,13 +15,12 @@ function renderProductos(lista, contenedorId) {
         const card = `
             <article class="col">
                 <section class="card h-100 shadow-sm">
-                    <img src="${prod.imagen}" class="card-img-top" alt="${prod.nombre}">
+                    <img src="${prod.imagen}" class="card-img-top" alt="${prod.titulo}">
                     <section class="card-body">
-                        <h5 class="card-title">${prod.nombre}</h5>
-                        <p class="card-text small">${prod.descripcion}</p>
+                        <h5 class="card-title">${prod.titulo}</h5>
                     </section>
                     <section class="card-footer d-flex justify-content-between align-items-center">
-                        <span class="fw-bold">S/. ${prod.precio.toFixed(2)}</span>
+                        <span class="fw-bold">S/ ${prod.precio.toFixed(2)}</span>
                         <button class="btn btn-sm btn-primary">Comprar</button>
                     </section>
                 </section>
@@ -36,48 +35,185 @@ function renderProductos(lista, contenedorId) {
 function actualizarBotonesPaginacion(totalProductos) {
     const btnAnterior = document.getElementById('btn-anterior');
     const btnSiguiente = document.getElementById('btn-siguiente');
-    
+
     btnAnterior.disabled = (paginaActual === 1);
     btnSiguiente.disabled = (paginaActual * productosPorPagina >= totalProductos);
 }
 
 function manejarFiltros() {
     const busqueda = document.getElementById('searchInput').value.toLowerCase();
-    const categoriaFiltro = document.getElementById('filterCategory').value;
+    const juegosFiltro = document.getElementById('filterJuegos').value;
     const precioFiltro = document.getElementById('filterPrice').value;
+    const psFiltro = document.getElementById('filterPlayStation').value;
+    const xboxFiltro = document.getElementById('filterXbox').value;
+    const serviciosFiltro = document.getElementById('filterServicios').value;
+    const ofertasFiltro = document.getElementById('filterOfertas').value;
+    const lanzamientosFiltro = document.getElementById('filterLanzamientos').value;
 
     let listaFiltrada = todosLosProductos.filter(p => {
-        const coincideBusqueda = p.nombre.toLowerCase().includes(busqueda) || 
-                                 p.descripcion.toLowerCase().includes(busqueda);
-        
-        let coincideCategoria = true;
-        if (categoriaFiltro !== "") {
-            switch (categoriaFiltro) {
-                case "consolas":
-                    coincideCategoria = (p.categoria === "Consolas");
+        const coincideBusqueda = p.titulo.toLowerCase().includes(busqueda) ||
+            p.descripcion_larga.toLowerCase().includes(busqueda);
+
+        let coincideJuegos = true;
+        if (juegosFiltro !== "") {
+            switch (juegosFiltro) {
+                case "accion":
+                    coincideJuegos = p.categorias.includes("ACCIÓN");
                     break;
-                case "juegos":
-                    coincideCategoria = (p.categoria === "Juegos");
+                case "aventura":
+                    coincideJuegos = p.categorias.includes("AVENTURA");
                     break;
-                case "accesorios":
-                    coincideCategoria = (p.categoria === "Accesorios");
+                case "arcade":
+                    coincideJuegos = p.categorias.includes("ARCADE");
                     break;
-                case "ps5":
-                    coincideCategoria = (p.categoria === "Juegos" && p.plataforma === "PS5");
+                case "conduccion-carreras":
+                    coincideJuegos = p.categorias.includes("CONDUCCIÓN y CARRERAS");
                     break;
-                case "xbox":
-                    coincideCategoria = (p.categoria === "Juegos" && p.plataforma === "Xbox Series X");
+                case "deportes":
+                    coincideJuegos = p.categorias.includes("DEPORTES");
                     break;
-                case "switch":
-                    coincideCategoria = (p.categoria === "Juegos" && p.plataforma === "Nintendo Switch");
+                case "ingenio":
+                    coincideJuegos = p.categorias.includes("INGENIO");
                     break;
-                case "pc":
-                    coincideCategoria = (p.categoria === "Juegos" && p.plataforma === "PC");
+                case "pelea":
+                    coincideJuegos = p.categorias.includes("LUCHA / PELEA");
+                    break;
+                case "plataforma":
+                    coincideJuegos = p.categorias.includes("PLATAFORMA");
+                    break;
+                case "shooter":
+                    coincideJuegos = p.categorias.includes("SHOOTER");
+                    break;
+                case "terror":
+                    coincideJuegos = p.categorias.includes("TERROR");
+                    break;
+                case "unico":
+                    coincideJuegos = p.categorias.includes("ÚNICO");
+                    break;
+                case "retro":
+                    coincideJuegos = p.categorias.includes("CLÁSICO / RETRO");
+                    break;
+                case "VR":
+                    coincideJuegos = p.categorias.includes("JUEGOS VR");
+                    break;
+                case "multijugador":
+                    coincideJuegos = p.categorias.includes("MULTIJUGADOR LOCAL / PANTALLA DIVIDIDA");
+                    break;
+                case "retrocompatible":
+                    coincideJuegos = p.categorias.includes("RETROCOMPATIBLE");
                     break;
             }
         }
-        
-        return coincideBusqueda && coincideCategoria;
+
+        let coincidePS = true;
+        if (psFiltro !== "") {
+            switch (psFiltro) {
+                case "PS5":
+                    coincidePS = p.categorias.includes("PlayStation 5") || p.categorias.includes("PLAYSTATION 5");
+                    break;
+                case "PS4":
+                    coincidePS = p.categorias.includes("PlayStation 4") || p.categorias.includes("PLAYSTATION 4");
+                    break;
+                case "PS3":
+                    coincidePS = p.categorias.includes("PLAYSTATION 3");
+                    break;
+                case "DELUXE":
+                    coincidePS = p.categorias.includes("Extra | Deluxe | Premium");
+                    break;
+                case "PS-PLUS":
+                    coincidePS = p.categorias.includes("PS Plus | PlayStation Plus");
+                    break;
+                case "PS-STORE":
+                    coincidePS = p.categorias.includes("PS Store | PlayStation Store");
+                    break;
+                case "PS-CARDS":
+                    coincidePS = p.categorias.includes("PSN Cards - PlayStation");
+                    break;
+            }
+        }
+
+        let coincideXBOX = true;
+        if (xboxFiltro !== "") {
+            switch (xboxFiltro) {
+                case "XBOX-X/S":
+                    coincideXBOX = p.categorias.includes("Xbox Series X|S");
+                    break;
+                case "XBOX-O":
+                    coincideXBOX = p.categorias.includes("Xbox One");
+                    break;
+                case "XBOX-LIVE":
+                    coincideXBOX = p.categorias.includes("XBOX LIVE");
+                    break;
+                case "XBOX-GAME-PASS":
+                    coincideXBOX = p.categorias.includes("Xbox Game Pass Ultimate");
+                    break;
+                case "XBOX-GIFT-CARDS":
+                    coincideXBOX = p.categorias.includes("Xbox Gift Cards");
+                    break;
+            }
+        }
+
+        let coincideServicios = true;
+        if (serviciosFiltro !== "") {
+            switch (serviciosFiltro) {
+                case "streaming":
+                    coincideServicios = p.categorias.includes("STREAMING");
+                    break;
+                case "recargas":
+                    coincideServicios = p.categorias.includes("RECARGAS");
+                    break;
+                case "tarjetas":
+                    coincideServicios = p.categorias.includes("TARJETAS");
+                    break;
+            }
+        }
+
+        let coincideOfertas = true;
+        if (ofertasFiltro !== "") {
+            switch (ofertasFiltro) {
+                case "ofertas-semana":
+                    coincideOfertas = p.categorias.includes("OFERTAS DE LA SEMANA");
+                    break;
+                case "end-of-year-PS4":
+                    coincideOfertas = p.categorias.includes("END OF YEAR - PS4");
+                    break;
+                case "end-of-year-PS5":
+                    coincideOfertas = p.categorias.includes("END OF YEAR - PS5");
+                    break;
+                case "ofertas-end-of-year":
+                    coincideOfertas = p.categorias.includes("OFERTAS END OF YEAR");
+                    break;
+                case "combos":
+                    coincideOfertas = p.categorias.includes("COMBOS");
+                    break;
+            }
+        }
+
+        let coincideLanzamientos = true;
+        if (lanzamientosFiltro !== "") {
+            switch (lanzamientosFiltro) {
+                case "estrenosPS5":
+                    coincideLanzamientos = p.categorias.includes("ESTRENOS PS5");
+                    break;
+                case "estrenosXBOX":
+                    coincideLanzamientos = p.categorias.includes("ESTRENOS XBOX");
+                    break;
+                case "nuevosLanzamientos":
+                    coincideLanzamientos = p.categorias.includes("NUEVOS LANZAMIENTOS");
+                    break;
+                case "preOrder-PS5":
+                    coincideLanzamientos = p.categorias.includes("PRE-ORDER PS5");
+                    break;
+                case "preOrder-XBOX":
+                    coincideLanzamientos = p.categorias.includes("PRE-ORDER XBOX");
+                    break;
+                case "proximosLanzamientos":
+                    coincideLanzamientos = p.categorias.includes("PRÓXIMOS LANZAMIENTOS");
+                    break;
+            }
+        }
+
+        return coincideBusqueda && coincideJuegos && coincidePS && coincideXBOX && coincideServicios && coincideOfertas && coincideLanzamientos;
     });
 
     if (precioFiltro === "asc") {
@@ -93,8 +229,13 @@ function manejarFiltros() {
 
 function limpiarFiltros() {
     document.getElementById('searchInput').value = '';
-    document.getElementById('filterCategory').value = '';
+    document.getElementById('filterJuegos').value = '';
     document.getElementById('filterPrice').value = '';
+    document.getElementById('filterPlayStation').value = '';
+    document.getElementById('filterXbox').value = '';
+    document.getElementById('filterServicios').value = '';
+    document.getElementById('filterOfertas').value = '';
+    document.getElementById('filterLanzamientos').value = '';
     manejarFiltros();
 }
 
@@ -106,7 +247,12 @@ async function cargarProductos() {
 
         renderProductos(productosFiltrados, "seccion-productos");
 
-        document.getElementById('filterCategory').addEventListener('change', manejarFiltros);
+        document.getElementById('filterJuegos').addEventListener('change', manejarFiltros);
+        document.getElementById('filterPlayStation').addEventListener('change', manejarFiltros);
+        document.getElementById('filterXbox').addEventListener('change', manejarFiltros);
+        document.getElementById('filterServicios').addEventListener('change', manejarFiltros);
+        document.getElementById('filterOfertas').addEventListener('change', manejarFiltros);
+        document.getElementById('filterLanzamientos').addEventListener('change', manejarFiltros);
         document.getElementById('filterPrice').addEventListener('change', manejarFiltros);
         document.getElementById('searchInput').addEventListener('input', manejarFiltros);
 
